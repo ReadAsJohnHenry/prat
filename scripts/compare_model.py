889e4cae8418b1cc7257ba638ebcf6484c95bc6d
Zhang <zhang.harn.yuu@gmail.com>
2026-01-25 23:58:36 +0100

    modify compare model

--- scripts/compare_model.py
+++ scripts/compare_model.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
 import torch
 from pix2rep.models.U_Net_CL import UNet, AttentionUNet
 
